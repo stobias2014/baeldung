c8ce5e4855817abc8acaa19a40f0f56f67bdd1a1
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-21 19:49:49 -0500

    added spring cloud config server project

---
+++ spring-cloud-config-server-baeldung/src/main/java/com/tobias/saul/springcloudconfigserverbaeldung/SpringCloudConfigServerBaeldungApplication.java
@@ -0,0 +1,13 @@
+package com.tobias.saul.springcloudconfigserverbaeldung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigServerBaeldungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerBaeldungApplication.class, args);
+	}
+
+}
