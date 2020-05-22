75573de73bad6b8a5e617355cc0daae090f9b043
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-21 19:57:37 -0500

    added .gitignore

--- spring-cloud-config-server-baeldung/src/main/java/com/tobias/saul/springcloudconfigserverbaeldung/SpringCloudConfigServerBaeldungApplication.java
+++ spring-cloud-config-server-baeldung/src/main/java/com/tobias/saul/springcloudconfigserverbaeldung/SpringCloudConfigServerBaeldungApplication.java
@@ -2,8 +2,10 @@ package com.tobias.saul.springcloudconfigserverbaeldung;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SpringCloudConfigServerBaeldungApplication {
 
 	public static void main(String[] args) {
