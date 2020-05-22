45994b4f0ba8f079522de174f0cf90734b090c68
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-21 20:38:52 -0500

    added rest controller

--- baeldung-config-client/src/main/java/com/tobias/saul/baeldungconfigclient/BaeldungConfigClientApplication.java
+++ baeldung-config-client/src/main/java/com/tobias/saul/baeldungconfigclient/BaeldungConfigClientApplication.java
@@ -1,13 +1,27 @@
 package com.tobias.saul.baeldungconfigclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BaeldungConfigClientApplication {
+	
+	@Value("${user.role}")
+	private String role;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BaeldungConfigClientApplication.class, args);
+	}
+	
+	@GetMapping("/whoami/{username}")
+	public String getRole(@PathVariable("username") String username) {
+		String message = "Hello " + username + " you are " + role;
+		return message;
 	}
 
 }
