a986ec0602ec000859c5b7e77ae0ce65061f5cb8
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-21 20:23:52 -0500

    added config client

---
+++ baeldung-config-client/src/main/java/com/tobias/saul/baeldungconfigclient/BaeldungConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.tobias.saul.baeldungconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeldungConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaeldungConfigClientApplication.class, args);
+	}
+
+}
---
+++ baeldung-config-client/src/test/java/com/tobias/saul/baeldungconfigclient/BaeldungConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobias.saul.baeldungconfigclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeldungConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
